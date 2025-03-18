49fb8d60d4777462a630f5ccca373753e488b521
jpcolli3 <jpcolli3@ncsu.edu>
2025-03-18 13:31:13 -0400

    new file for custom loss function and placeholder model

--- CNN_model.py
+++ CNN_model.py
@@ -2,9 +2,14 @@ import torch
 import torch.nn as nn
 
 class CNN_Model(nn.Module):
-    def __init__(self):
+    def __init__(self, batch_size):
         super().__init__()
-        self.input = 
+        self.batch_size = batch_size
+        self.input = nn.Linear(1024, 512) #placeholder architecture with linear layers
+        self.output = nn.Linear(512, 7)
 
-    def forward(x):
+    def forward(self, x):
+        x = x.reshape(self.batch_size, 1, 1024)
+        x = self.input(x)
+        x = self.output(x)
         return x
---
+++ mtl_loss.py
@@ -0,0 +1,6 @@
+import torch.nn as nn
+
+def MTL_Loss():
+
+    return 1
+    
