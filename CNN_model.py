7f4407e02c8659145d390510d42f6ea4c5b18578
jpcolli3 <jpcolli3@ncsu.edu>
2025-03-18 14:46:06 -0400

    push commit for end of day

--- CNN_model.py
+++ CNN_model.py
@@ -6,10 +6,39 @@ class CNN_Model(nn.Module):
         super().__init__()
         self.batch_size = batch_size
         self.input = nn.Linear(1024, 512) #placeholder architecture with linear layers
-        self.output = nn.Linear(512, 5)
+        self.shared = nn.Sequential(
+            nn.Linear(512,256),
+            nn.Linear(256,128),
+            nn.Linear(128,64),
+        )
+        self.output = nn.Linear(64,5)
+
+        # self.num_pulses = nn.Sequential(
+        #     nn.Conv1d(),
+        #     nn.Linear()
+        # )
+        # self.pulse_width = nn.Sequential(
+        #     nn.Conv1d(),
+        #     nn.Linear()
+        # )
+        # self.time_delay = nn.Sequential(
+        #     nn.Conv1d(),
+        #     nn.Linear()
+        # )
+        # self.repetition_interval = nn.Sequential(
+        #     nn.Conv1d(),
+        #     nn.Linear()
+        # )
+        # self.classifier = nn.Sequential(
+        #     nn.Conv1d(),
+        #     nn.Linear(),
+        #     nn.Softmax()
+        # )
+
 
     def forward(self, x):
         x = x.reshape(self.batch_size, 1024)
         x = self.input(x)
+        x = self.shared(x)
         x = self.output(x)
         return x
